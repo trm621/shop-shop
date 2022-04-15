c2c8044f088a2ad8e0d71f93df5231abd15144a4
trm621 <t.maher621@gmail.com>
2022-04-15 16:41:34 -0400

    set up project

---
+++ client/build/precache-manifest.a4c8d70e4ec3b2cd3a97cb2f2d15f332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f0a06e10dd5c1d98c2b41859d658b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc5daa7b597906271db",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "954caf182929c47e2c54",
+    "url": "/static/js/2.288c6b8b.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.288c6b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc5daa7b597906271db",
+    "url": "/static/js/main.7a8f9f83.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
